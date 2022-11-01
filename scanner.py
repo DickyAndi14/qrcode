419fb9c8d59bc25c2a575a0183bd17e46d976852
Dicky Andi Soffan <dickyandi.soffan@gmail.com>
2022-11-01 18:51:37 +0700

    qrcode

---
+++ scanner.py
@@ -0,0 +1,22 @@
+import cv2
+import pyzbar.pyzbar as pyzbar
+
+img = cv2.imread('channel.png')
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    cv2.imshow("Frame", frame)
+
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        # print(obj.data.decode('utf-8'))
+
+        if len(obj) >= 1:
+            import webbrowser
+            webbrowser.open(obj.data.decode('utf-8'))
+            exit()
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
